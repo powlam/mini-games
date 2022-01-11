6bc4a28bcac7528e4a0af8899e9479adc0b56dfb
Paul Albandoz <palbandoz@gmail.com>
2022-01-11 16:57:56 +0100

    package akaunting/laravel-language

--- config/app.php
+++ config/app.php
@@ -213,6 +213,7 @@ return [
         'Http' => Illuminate\Support\Facades\Http::class,
         'Js' => Illuminate\Support\Js::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
+        'Language' => Akaunting\Language\Facade::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [AppController::class, 'welcome']);
-Route::get('/secret-word', [AppController::class, 'secretWord']);
+Route::group(['middleware' => 'language'], function () {
+    Route::get('/', [AppController::class, 'welcome']);
+    Route::get('/secret-word', [AppController::class, 'secretWord']);
+});
