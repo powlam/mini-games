4890459aa402c352297c56e2af2554366c486236
Paul Albandoz <palbandoz@gmail.com>
2022-01-11 14:53:29 +0100

    secret-word input[text] always focused

--- resources/views/livewire/word.blade.php
+++ resources/views/livewire/word.blade.php
@@ -5,10 +5,8 @@
 
     <x-word :word="$word"/>
 
-    <input type="text" wire:model="word" autofocus maxlength="{{ App\Models\SecretWord::LETTERS }}" class="uppercase">
-    <button wire:click="checkWord" class="bg-yellow-200 hover:bg-yellow-400 p-2 cursor-pointer block">
-        Check
-    </button>
+    <input type="text" wire:model="word" wire:keydown.enter="checkWord" wire:keydown.escape="resetError" autofocus onblur="this.focus()" maxlength="{{ App\Models\SecretWord::LETTERS }}" class="appearance-none border-0 outline-0 uppercase text-center text-blue-300">
+
     @if ($error)
         <div class="text-lg p-2 m-2 text-red-400 rounded border-2 border-red-400 cursor-pointer" wire:click="resetError">
             {{ $error }}
